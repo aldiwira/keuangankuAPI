1cd602038101a7e675c5361de794235d668d4aa4
aldiwira <aldiwira.aw@gmail.com>
2020-09-25 20:55:14 +0700

    add some middleware

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv')
-const Express = require('express')
+const express = require('express')
 const pjson = require('./package.json')
+const helmet = require('helmet')
+const cors = require('cors')
 const { db } = require('./db')
 
-const App = Express()
+const app = express()
 const port = 3000 || process.env.port
 const envStat = process.env.NODE_ENV
 
-App.get('/', async (req, res) => {
+//middleware
+app.use(helmet())
+app.use(cors())
+app.get('/', async (req, res) => {
   const AppTest = {
     App: `${pjson.name}`,
     Version: `${pjson.version}`
@@ -15,10 +20,24 @@ App.get('/', async (req, res) => {
   res.json(AppTest)
 })
 
+app.use(async (error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status)
+  } else {
+    res.status(500)
+  }
+  res.json({
+    code: error.status ? error.status : 500,
+    message: error.message,
+    data: false
+  })
+})
+
+//check data
 db.then(() => {
   console.log(envStat === 'development' ? 'Connected' : null)
 })
 
-App.listen(port, () => {
+app.listen(port, () => {
   console.log(envStat + ' Running at 3000')
 })
